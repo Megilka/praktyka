a6adb6d196bf173959052401361dfc5fbb32d2ca
Megilka <gmagdalena208@gmail.com>
2024-11-12 10:32:36 +0100

    simple faucet fix

---
+++ faucet.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: CC-BY-SA-4.0
+
+// Version of Solidity compiler this program was written for
+pragma solidity ^0.8.4;
+
+// Pierwszy kontrakt działa jak kran.
+contract Faucet {
+    // Przekazuje ethery na dowolny wskazany adres.
+    function withdraw(uint withdraw_amount) public {
+        // Ograniczanie żądanej kwoty.
+        require(withdraw_amount <= 100000000000000000);
+        // Przekazywanie środków na wskazany adres.
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+    // Przyjmuje dowolną przesyłaną kwotę.
+    receive () external payable {}
+}
